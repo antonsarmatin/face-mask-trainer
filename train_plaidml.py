90445cfeedcad7dc7f951af73f92323bc28f7ee3
a.sarmatin <a.sarmatin@cometrica.ru>
2020-06-17 14:10:40 +0300

    trainer project init

---
+++ train_plaidml.py
@@ -0,0 +1,143 @@
+# USAGE
+# python train_mask_detector.py --dataset dataset
+
+# construct the argument parser and parse the arguments
+import argparse
+import os
+
+import plaidml.keras
+
+plaidml.keras.install_backend()
+os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+
+from imutils import paths
+import keras
+import keras.backend as K
+from keras.layers import AveragePooling2D, Flatten, Dense, Dropout, Input
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.preprocessing.image import load_img, ImageDataGenerator
+from keras.utils import to_categorical
+from keras.applications.mobilenet_v2 import MobileNetV2
+from keras.applications.mobilenet_v2 import preprocess_input
+from keras_preprocessing.image import img_to_array
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", default="dataset/",
+                help="path to input dataset")
+ap.add_argument("-p", "--plot", type=str, default="plot.png",
+                help="path to output loss/accuracy plot")
+ap.add_argument("-m", "--model", type=str,
+                default="mask_detector_plaidml.model",
+                help="path to output face mask detector model")
+args = vars(ap.parse_args())
+
+INIT_LR = 1e-4
+EPOCHS = 20
+BS = 32
+
+print("[INFO] loading images...")
+imagePaths = list(paths.list_images(args["dataset"]))
+print("[INFO] loaded ", len(imagePaths), " images")
+data = []
+labels = []
+
+for imagePath in imagePaths:
+    # extract the class label from the filename
+    label = imagePath.split(os.path.sep)[-2]
+
+    # load the input image (224x224) and preprocess it
+    image = load_img(imagePath, target_size=(224, 224))
+    image = img_to_array(image)
+    image = preprocess_input(image)
+
+    # update the data and labels lists, respectively
+    data.append(image)
+    labels.append(label)
+
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = to_categorical(labels)
+
+# partition the data into training and testing splits using 75% of
+# the data for training and the remaining 25% for testing
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+                                                  test_size=0.20, stratify=labels, random_state=42)
+print("[INFO] trainX: ", len(trainX), " trainY:", len(trainY))
+
+aug = ImageDataGenerator(
+    rotation_range=20,
+    zoom_range=0.15,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.15,
+    horizontal_flip=True,
+    fill_mode="nearest")
+
+baseModel = MobileNetV2(weights="imagenet", include_top=False,
+                        input_tensor=Input(shape=(224, 224, 3)))
+
+headModel = baseModel.output
+headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(128, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+headModel = Dense(2, activation="softmax")(headModel)
+
+model = Model(inputs=baseModel.input, outputs=headModel)
+
+for layer in baseModel.layers:
+    layer.trainable = False
+
+# compile our model
+print("[INFO] compiling model...")
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt,
+              metrics=["accuracy"])
+
+# train the head of the network
+print("[INFO] training head...")
+H = model.fit(
+    trainX, trainY,
+    steps_per_epoch=len(trainX) // BS,
+    validation_data=(testX, testY),
+    validation_steps=len(testX) // BS,
+    epochs=EPOCHS)
+
+# make predictions on the testing set
+print("[INFO] evaluating network...")
+predIdxs = model.predict(testX, batch_size=BS)
+
+# for each image in the testing set we need to find the index of the
+# label with corresponding largest predicted probability
+predIdxs = np.argmax(predIdxs, axis=1)
+
+# show a nicely formatted classification report
+print(classification_report(testY.argmax(axis=1), predIdxs,
+                            target_names=lb.classes_))
+
+# serialize the model to disk
+print("[INFO] saving mask detector model...")
+model.save(args["model"])
+
+# plot the training loss and accuracy
+N = EPOCHS
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig(args["plot"])
