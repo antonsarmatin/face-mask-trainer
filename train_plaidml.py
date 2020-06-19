d5d68550e1d99d31f767350a5b3845b20e9b28bb
a.sarmatin <a.sarmatin@cometrica.ru>
2020-06-19 13:14:25 +0300

    train algorithm fine tune

--- train_plaidml.py
+++ train_plaidml.py
@@ -105,8 +105,8 @@ model.compile(loss="binary_crossentropy", optimizer=opt,
 
 # train the head of the network
 print("[INFO] training head...")
-H = model.fit(
-    trainX, trainY,
+H = model.fit_generator(
+    aug.flow(trainX, trainY, batch_size=BS),
     steps_per_epoch=len(trainX) // BS,
     validation_data=(testX, testY),
     validation_steps=len(testX) // BS,
@@ -128,16 +128,3 @@ print(classification_report(testY.argmax(axis=1), predIdxs,
 print("[INFO] saving mask detector model...")
 model.save(args["model"])
 
-# plot the training loss and accuracy
-N = EPOCHS
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
-plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
-plt.title("Training Loss and Accuracy")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend(loc="lower left")
-plt.savefig(args["plot"])
